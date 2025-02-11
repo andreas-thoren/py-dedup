a6d247b607dd284e879fdf813f2da2d29cb92b4f
Andreas Thorén <athoren79@yahoo.com>
2025-02-11 20:44:55 +0100

    black

--- tests/test_persistent_cache.py
+++ tests/test_persistent_cache.py
@@ -1,19 +1,15 @@
 import pathlib
 import unittest
 from py_dedup.persistent_cache import *
-from tests.global_test_vars import (
-    TEST_DIR,
-    CMPR_DIR,
-    COPY_CMPR_DIR
-)
+from tests.global_test_vars import TEST_DIR, CMPR_DIR, COPY_CMPR_DIR
 
 
 class TestPersistentCache(unittest.TestCase):
     def test_get_tempfile_prefix(self):
         cwd = pathlib.Path.cwd()
         absolute_paths = [TEST_DIR, CMPR_DIR, COPY_CMPR_DIR]
-        relative_paths = [ path.relative_to(cwd) for path in absolute_paths ]
-        parent_paths = [ path.parent for path in relative_paths ]
+        relative_paths = [path.relative_to(cwd) for path in absolute_paths]
+        parent_paths = [path.parent for path in relative_paths]
         reverse_paths = relative_paths[::-1]
 
         prefix_absolute = get_tempfile_prefix(absolute_paths)
@@ -23,10 +19,10 @@ class TestPersistentCache(unittest.TestCase):
 
         # Same prefix is expected no matter if paths are relative or absolute
         self.assertEqual(prefix_absolute, prefix_relative)
-        
+
         # Same prefix is expected no matter the order
         self.assertEqual(prefix_absolute, prefix_reversed)
-        
+
         # Different actual dirs should result in different prefixes
         self.assertNotEqual(prefix_relative, prefix_parents)
 
